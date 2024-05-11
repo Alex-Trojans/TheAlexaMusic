58be66a89d323879797c9720920d188301765db6
ALEXANDER <107063122+Alex-Trojans@users.noreply.github.com>
2024-05-12 00:20:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Alex-Trojans/TrojansTanusolo",
+    "https://github.com/Alex-Trojans/TheAlexaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
